57bb9e129c534ac39411e6b7a1bb61dfca4b8d34
Leonnardo Rabello <leonnardo@gmail.com>
2024-12-04 23:20:35 -0300

    Add neogit and change gitsigns config

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,1 +1,24 @@
-return { 'lewis6991/gitsigns.nvim' }
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+			end,
+		},
+	},
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim",
+	},
+	config = true,
+}
