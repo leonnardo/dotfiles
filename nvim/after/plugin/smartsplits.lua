a8a7a4d26c927112fb362ee146567a33b6bc340d
Leonnardo Rabello <lrabello@MacBook-Pro.local>
2024-12-23 17:10:05 -0300

    Fix movement between nvim and tmux

---
+++ nvim/after/plugin/smartsplits.lua
@@ -0,0 +1,6 @@
+-- moving between splits
+vim.keymap.set("n", "<C-left>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-down>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-up>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-righ>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
