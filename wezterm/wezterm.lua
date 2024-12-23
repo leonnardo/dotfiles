833ac32dd53a35a8eed166934f4ee565c75daabe
Leonnardo Rabello <lrabello@MacBook-Pro.local>
2024-12-23 16:46:56 -0300

    Changes in wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,44 @@
 local wezterm = require("wezterm")
 
-local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
+local tmux_prefix = function(key)
+	return wezterm.action.Multiple({
+		wezterm.action.SendKey({ mods = "CTRL", key = "a" }),
+		wezterm.action.SendKey({ key = key }),
+	})
 end
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "DemiBold" })
-config.font_size = 15.0
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.90
-config.macos_window_background_blur = 25
-config.native_macos_fullscreen_mode = false
+local ctrl_prefix = function(key)
+	return wezterm.action.SendKey({ mods = "CTRL", key = key })
+end
 
-config.keys = {
-	-- {
-	--   key = 'A',
-	--   mods = 'CTRL|SHIFT',
-	--   action = wezterm.action.QuickSelect,
-	-- }
+return {
+	-- default_prog = { "tmux", "new", "-A" },
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "DemiBold" }),
+	font_size = 15.0,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.97,
+	window_close_confirmation = "NeverPrompt",
+	native_macos_fullscreen_mode = false,
+	keys = {
+		-- { mods = "SUPER", key = "", action = },
+		{ mods = "SUPER", key = "t", action = tmux_prefix("t") },
+		{ mods = "SUPER", key = "w", action = tmux_prefix("w") },
+		{ mods = "SUPER", key = "k", action = tmux_prefix("s") },
+		{ mods = "SUPER", key = "s", action = tmux_prefix("%") },
+		{ mods = "SUPER", key = "S", action = tmux_prefix("-") },
+		{ mods = "SUPER", key = "Z", action = tmux_prefix("z") },
+		{ mods = "SUPER", key = "r", action = ctrl_prefix("r") },
+		{ mods = "SUPER", key = "1", action = ctrl_prefix("1") },
+		{ mods = "SUPER", key = "2", action = ctrl_prefix("2") },
+		{ mods = "SUPER", key = "3", action = ctrl_prefix("3") },
+		{ mods = "SUPER", key = "4", action = ctrl_prefix("4") },
+		{ mods = "SUPER", key = "5", action = ctrl_prefix("5") },
+		{ mods = "SUPER", key = "e", action = tmux_prefix("n") },
+		{ mods = "SUPER", key = "n", action = tmux_prefix("p") },
+		{ mods = "SUPER", key = "LeftArrow", action = ctrl_prefix("LeftArrow") },
+		{ mods = "SUPER", key = "DownArrow", action = ctrl_prefix("DownArrow") },
+		{ mods = "SUPER", key = "UpArrow", action = ctrl_prefix("UpArrow") },
+		{ mods = "SUPER", key = "RightArrow", action = ctrl_prefix("RightArrow") },
+	},
 }
-
-return config
