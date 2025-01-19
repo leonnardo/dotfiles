e4406fb91643f68aa98c182839b59b9c1c733d00
Leonnardo Rabello <me@lrabello.dev>
2025-01-19 20:13:25 -0300

    Improve formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,50 +1,50 @@
-return { -- Autoformat
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format()
-			end,
-			mode = "",
-			desc = "[F]ormat buffer",
-		},
-	},
-	opts = function()
-		vim.api.nvim_create_user_command("ToggleAutoformat", function(args)
-			local opts = vim.g
-			local toggle_for_buffer = false
-			if args.bang then
-				opts = vim.b
-				toggle_for_buffer = true
-			end
-			opts.disable_autoformat = not opts.disable_autoformat
-			if toggle_for_buffer then
-				Snacks.notify.info("Autoformat for buffer " .. (opts.disable_autoformat and "disabled" or "enabled"))
-			else
-				Snacks.notify.info("Autoformat " .. (opts.disable_autoformat and "disabled" or "enabled"))
-			end
-		end, {
-			desc = "Toggle Autoformat on save",
-			bang = true,
-		})
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        require("conform").format()
+      end,
+      mode = "",
+      desc = "[F]ormat buffer",
+    },
+  },
+  opts = function()
+    vim.api.nvim_create_user_command("ToggleAutoformat", function(args)
+      local opts = vim.g
+      local toggle_for_buffer = false
+      if args.bang then
+        opts = vim.b
+        toggle_for_buffer = true
+      end
+      opts.disable_autoformat = not opts.disable_autoformat
+      if toggle_for_buffer then
+        Snacks.notify.info("Autoformat for buffer " .. (opts.disable_autoformat and "disabled" or "enabled"))
+      else
+        Snacks.notify.info("Autoformat " .. (opts.disable_autoformat and "disabled" or "enabled"))
+      end
+    end, {
+      desc = "Toggle Autoformat on save",
+      bang = true,
+    })
 
-		return {
-			notify_on_error = true,
-			format_on_save = function(bufnr)
-				-- Disable with a global or buffer-local variable
-				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-					return
-				end
-				return { timeout_ms = 500, lsp_format = "fallback" }
-			end,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-				html = { "prettier" },
-			},
-		}
-	end,
+    return {
+      notify_on_error = true,
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+        html = { "prettier" },
+      },
+    }
+  end,
 }
