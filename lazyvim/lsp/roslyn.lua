5bee9aa8121df82afa5ffb47070ea95af203b45a
Leonnardo Rabello <leonnardo@gmail.com>
2025-09-15 00:52:57 -0300

    Update neovim lsp config

--- lazyvim/lsp/roslyn.lua
+++ lazyvim/lsp/roslyn.lua
@@ -10,7 +10,7 @@ return {
   },
   settings = {
     ["csharp|code_lens"] = {
-      dotnet_enable_references_code_lens = true,
+      dotnet_enable_references_code_lens = false,
     },
     ["csharp|background_analysis"] = {
       dotnet_analyzer_diagnostics_scope = "openFiles",
