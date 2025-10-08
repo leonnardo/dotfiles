49cd3dd2b15054578178a4a1d7fb07423b7502fa
Leonnardo <leonnardo@gmail.com>
2025-10-07 23:11:18 -0300

    Add mise eval

--- zsh/4.eval.zsh
+++ zsh/4.eval.zsh
@@ -1,5 +1,6 @@
 (( $+commands[fzf] )) && eval "$(fzf --zsh)"
 (( $+commands[zoxide] )) && eval "$(zoxide init --cmd ${ZOXIDE_CMD_OVERRIDE:-z} zsh)"
+(( $+commands[mise] )) && eval "$(mise activate zsh)"
 (( $+commands[oh-my-posh] )) && eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/themes/lrabello.toml)"
 (( $+commands[flux] )) && eval "$(flux completion zsh)"
 (( $+commands[mise] )) && eval "$(mise activate zsh)"
